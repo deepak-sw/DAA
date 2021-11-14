203be921447a7cc104e2f992492652b730508d01
Deepak Singh Karki <87329265+deepak-sw@users.noreply.github.com>
2021-11-14 22:24:40 +0530

    Update program3.cpp

--- Week5/program3.cpp
+++ Week5/program3.cpp
@@ -1,29 +1,40 @@
 #include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
 using namespace std;
+
 int main()
 {
-    // ios_base::sync_with_stdio(false);
-    // cin.tie(NULL);
-    int a, b;
-    cin >> a >> b;
-    int arr[a], brr[b];
-    for (int i = 0; i < a; i++)
-        cin >> arr[i];
-    for (int i = 0; i < b; i++)
-        cin >> brr[i];
-    int p1 = 0, p2 = 0;
-    while (p1 < a && p2 < b)
-    {
-        if (arr[p1] == brr[p2])
-        {
-            cout << arr[p1] << " ";
-            p1++;
-            p2++;
-        }
-        else if (arr[p1] < brr[p2])
-            p1++;
+    fastio;
+    ifstream file;
+    file.open("input_3.txt");
+    if(!file){
+        cout<<"File not found!"<<endl;
+        return 0;
+    }
+    int n,i,j;
+    file>>n;
+    int arr1[n];
+    for(i=0; i<n; i++)
+        file>>arr1[i];
+    int m;
+    file>>m;
+    int arr2[m];
+    for(j=0;j<m;j++)    
+        file>>arr2[j];
+    i=0;j=0;
+    while(i<n && j<m){
+        if(arr1[i] == arr2[j])
+            {cout<<arr1[i++]<<" ";}
+        else if(arr1[i]<arr2[j])
+            i++;
         else
-            p2++;
+            j++;
     }
     return 0;
 }
