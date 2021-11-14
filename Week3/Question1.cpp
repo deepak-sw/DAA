ead5f2543193b7a492a87e4d35eeb743b6d5cd40
Deepak Singh Karki <87329265+deepak-sw@users.noreply.github.com>
2021-11-14 21:59:09 +0530

    Update Question1.cpp

--- Week3/Question1.cpp
+++ Week3/Question1.cpp
@@ -1,38 +1,89 @@
 #include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
 using namespace std;
+
+void merge(int* arr, int l,int m,int r){
+    int n1=m-l+1,n2=r-m;
+    int L[n1],R[n2],i,j,k;
+    for(i=0;i<n1;i++)L[i]=arr[l+i];
+    for(j=0;j<n2;j++)R[j]=arr[m+1+j];
+    i=0;j=0;k=l;
+    while(i<n1 && j<n2){
+        if(L[i]<=R[j]){
+            arr[k] = L[i];
+            i++;
+        }
+        else{
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<n1){
+        arr[k]= L[i];
+        i++;
+        k++;
+    }
+    while(j<n2){
+        arr[k]= R[j];
+        j++;
+        k++;
+    }
+}
+
+void Sort(int* arr, int l, int h){
+    if(l<h){
+        int mid = l+(h-l)/2;
+        Sort(arr,l,mid);
+        Sort(arr,mid+1,h);
+        merge(arr,l,mid,h);
+    }
+}
+
+int BinarySearch(int* arr, int l, int h){
+    int mid,flag=1;
+    while(h>=l){
+        mid=(h+l)/2;
+        if(arr[mid]==arr[mid+1]){
+            flag=0;break;
+        }else if(arr[mid]>arr[mid+1])
+            h=mid-1;
+        else if(arr[mid]<arr[mid+1])
+            l=mid+1;
+    }
+    return flag;
+}
+
 int main()
 {
-    int n, t, temp, shifts, comp;
-    cin >> t;
-    while (t--)
-    {
-        shifts = comp = 0;
-        cin >> n;
+    fastio;
+    int t;
+    ifstream file;
+    file.open("input_3.txt");
+    if(!file){
+        cout<<"File not found";
+        return 0;
+    }
+    file>>t;
+    while(t--){
+        int n;
+        file>>n;
         int arr[n];
         for (int i = 0; i < n; i++)
-            cin >> arr[i];
-
-        for (int i = 1; i < n; i++)
-        {
-            int j = i;
-            comp++;
-            while ((j > 0) && (arr[j - 1] > arr[j]))
-            {
-                if (arr[j - 1] > arr[j])
-                    comp++;
-                temp = arr[j - 1];
-                arr[j - 1] = arr[j];
-                arr[j] = temp;
-                j--;
-
-                shifts++;
-            }
-        }
-
-        for (int i = 0; i < n; i++)
-            cout << arr[i];
-        cout << "\ncomparisons  = " << comp;
-        cout << "\nshifts = " << shifts;
+            file>>arr[i];
+        Sort(arr,0,n-1);
+        int flag=BinarySearch(arr,0,n-1);
+        if(flag)
+            cout<<"NO"<<endl;
+        else
+            cout<<"YES"<<endl;
     }
+    file.close();
     return 0;
 }
