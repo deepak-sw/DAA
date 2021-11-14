dfc67daefb3468e1ab8baaeca339eb104bd3d669
Deepak Singh Karki <87329265+deepak-sw@users.noreply.github.com>
2021-11-14 22:23:28 +0530

    Update Question1.cpp

--- Week5/Question1.cpp
+++ Week5/Question1.cpp
@@ -1,27 +1,42 @@
 #include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
 using namespace std;
+
 int main()
 {
-    // ios_base::sync_with_stdio(false);
-    // cin.tie(NULL);
-    int t, n;
-    cin >> t;
-    string s;
-    while (t--)
-    {
-        vector<int> arr(26, 0);
-        cin >> n;
-        cin >> s;
-        for (int i = 0; i < n; i++)
-            arr[(int)(s[i]) - 97]++;
-        int res = arr[0];
-        for (int i = 1; i < 26; i++)
-            res = max(arr[i], res);
-        if(res == 1){
-            cout<<"No Duplicates Present\n";
-            continue;
+    fastio;
+    ifstream file;
+    file.open("input_1.txt");
+    if(!file){
+        cout<<"File not found!";
+        return 0;
+    }
+    int t;
+    file>>t;
+    while(t--){
+        int n;
+        file>>n;
+        char arr[n];
+        multimap<int,char> mpp;
+        map<char,int> freq;
+        for(int i=0; i<n; i++){
+            file>>arr[i];
+            freq[arr[i]]++;
         }
-        cout << res << endl;
+        int flag=0;
+        for(auto i: freq)
+            mpp.insert({i.second,i.first});
+        if(mpp.rbegin()->first>1){
+            cout<<mpp.rbegin()->second<<"-"<<mpp.rbegin()->first<<endl;
+            flag=1;}
+        if(!flag)
+            cout<<"No Duplicate Present"<<endl;
     }
     return 0;
 }
