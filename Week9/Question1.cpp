e40ab480282051240083a43bef79fc14e4043f0d
Deepak Singh Karki <87329265+deepak-sw@users.noreply.github.com>
2021-11-14 23:20:13 +0530

    Update Question1.cpp

--- Week9/Question1.cpp
+++ Week9/Question1.cpp
@@ -1,41 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
-// find shortest path using floyd warshall algorithm
-int main()
-{
-    int n,i,j,k,w;
-    cout<<"Enter the number of vertices: ";
-    cin>>n;
-    int graph[n][n];
-    cout<<"Enter the adjacency matrix: "<<endl;
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<n;j++)
-        {
-            cin>>graph[i][j];
+
+#define INF 1e9
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input_1.txt", "r", stdin);
+    freopen("output_1.txt", "w", stdout);
+#endif
+    int n;
+    // cout << "for values INF enter -1" << endl;
+    cin >> n;
+    int a;
+    int arr[n][n], dist[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> a;
+            if (a < 0) {
+                arr[i][j] = INF;
+            }
+            else
+                arr[i][j] = a;
+            dist[i][j] = arr[i][j];
         }
     }
-    for(k=0;k<n;k++)
-    {
-        for(i=0;i<n;i++)
-        {
-            for(j=0;j<n;j++)
-            {
-                if(graph[i][k]+graph[k][j]<graph[i][j])
-                {
-                    graph[i][j]=graph[i][k]+graph[k][j];
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
                 }
             }
         }
     }
-    cout<<"The shortest path matrix is: "<<endl;
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<n;j++)
-        {
-            cout<<graph[i][j]<<" ";
+    cout << "Shortest Distance Matrix: " << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] == INF) {
+                cout << "INF ";
+            }
+            else
+                cout << dist[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
