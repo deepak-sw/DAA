6fc66b051eb33139ec2223b93764c656ae16681d
Deepak Singh Karki <87329265+deepak-sw@users.noreply.github.com>
2021-11-14 22:00:00 +0530

    Update Question2.cpp

--- Week3/Question2.cpp
+++ Week3/Question2.cpp
@@ -1,37 +1,56 @@
 #include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
 using namespace std;
+
+void printArray(int* arr,int n){
+    for(int i = 0; i < n; i++)
+        cout<<arr[i]<<" ";
+}
+
+void InsertionSort(int* arr, int n){
+    int key,i,j,comparisons=0,shifts=0;
+    for(int i=1;i<n;i++){
+        shifts++;
+        key = arr[i];
+        j = i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1] = arr[j];
+            j--;
+            comparisons++;
+            shifts++;
+        }
+        arr[j+1] = key;
+    }
+    printArray(arr,n);
+    cout<<endl<<"Comparisons: "<<comparisons<<endl;
+    cout<<"Shifts: "<<shifts<<endl;
+}
+
 int main()
 {
-    // ios_base::sync_with_stdio(0);
-    // cin.tie(NULL);
-    int t, n, comp, swaps;
-    cin >> t;
-    while (t--)
-    {
-        comp = swaps = 0;
-        cin >> n;
+    fastio;
+    int t;
+    ifstream file;
+    file.open("input_1.txt");
+    if(!file){
+        cout<<"File not found";
+        return 0;
+    }
+    file>>t;
+    while(t--){
+        int n;
+        file>>n;
         int arr[n];
         for (int i = 0; i < n; i++)
-            cin >> arr[i];
-        for (int i = 0; i < n - 1; i++)
-        {
-            for (int j = i + 1; j < n; j++)
-            {
-                comp++;
-                if (arr[i] > arr[j])
-                {
-                    swap(arr[i], arr[j]);
-                    swaps++;
-                }
-            }
-        }
-        for (int i = 0; i < n; i++)
-        {
-            cout << arr[i] << " ";
-        }
-        cout << "\ncomparisons = " << comp;
-        cout << "\nswaps = " << swaps << endl;
+            file>>arr[i];
+        InsertionSort(arr,n);
     }
-
+    file.close();
     return 0;
 }
