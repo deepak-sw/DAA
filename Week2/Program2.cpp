66a9c5d4383e8f9036fea56e9d5985dc5f863c5c
Deepak Singh Karki <87329265+deepak-sw@users.noreply.github.com>
2021-11-14 21:48:25 +0530

    Update Program2.cpp

--- Week2/Program2.cpp
+++ Week2/Program2.cpp
@@ -1,29 +1,45 @@
 #include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
 using namespace std;
+
 int main()
 {
-    // ios_base::sync_with_stdio(0);
-    // cin.tie(NULL);
-    int t, n, counts;
-    cin >> t;
-    while (t--)
-    {
-        cin >> n;
-        counts = 0;
-        vector<int> arr(n);
-        for (int i = 0; i < n; i++)
-            cin >> arr[i];
-
-        for (int i = 0; i < n - 1; i++)
-            for (int j = i + 1; j < n; j++)
-                if (binary_search(arr.begin() + j, arr.end(), arr[i] + arr[j]))
-                {
-                    cout << arr[i] << " " << arr[j] << " " << arr[i] + arr[j] << endl;
-                    counts++;
-                }
-        if (counts == 0)
-            cout << "No Sequence Found\n";
+    fastio;
+    int t;
+    ifstream file;
+    file.open("input_2.txt");
+    if(!file){
+        cout<<"File not found";
+        return 0;
     }
-
+    file>>t;
+    while(t--){
+        int n;
+        file>>n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            file>>arr[i];
+        int flag=0;
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j <n;j++){
+                for(int k = j+1; k < n; k++){
+                    if(arr[i] + arr[j] == arr[k]){
+                        flag=1;
+                        cout<<i+1<<","<<j+1<<","<<k+1<<endl;
+                        break;
+                    }
+                }
+            }
+        }
+        if(!flag)
+            cout<<"No sequence found!"<<endl;
+    }
+    file.close();
     return 0;
 }
