556c368d8bca14d144fcdeaf7972030f9abf7910
Deepak Singh Karki <87329265+deepak-sw@users.noreply.github.com>
2021-11-14 22:44:43 +0530

    Update Program2.cpp

--- Week6/Program2.cpp
+++ Week6/Program2.cpp
@@ -1,48 +1,57 @@
-// C++ program to find out whether a
-// given graph is Bipartite or not
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
 
 using namespace std;
-bool isBipartite(int v, int* G[], int src)
-{
-    int colorArr[v];
-    for (int i = 0; i < v; ++i)
-        colorArr[i] = -1;
 
-    colorArr[src] = 1;
-    queue<int> q;
-    q.push(src);
-    while (!q.empty())
-    {
-        int u = q.front();
-        q.pop();
-        if (G[u][u] == 1)
+bool isbipartite(vector<vector<int>>& graph, int s){
+    int V = graph.size();
+    vector<int> visited(V);
+    for(int i=0; i<V;i++)
+        visited[i] = -1;
+    visited[s] = 1;
+    queue<int> qu;
+    qu.push(s);
+    while(!qu.empty()){
+        int u = qu.front();
+        qu.pop();
+        if(graph[u][u] == 1)
             return false;
-
-        for (int v = 0; v < v; ++v)
-        {
-            if (G[u][v] && colorArr[v] == -1)
-            {
-                colorArr[v] = 1 - colorArr[u];
-                q.push(v);
+        for(int v=0; v<V;v++){
+            if(graph[u][v] && visited[v] == -1){
+                visited[v] = 1 - visited[u];
+                qu.push(v);
             }
-            else if (G[u][v] && colorArr[v] == colorArr[u])
+            else if(graph[u][v] && visited[v] == visited[u])
                 return false;
         }
     }
-
     return true;
 }
+
+
 int main()
 {
-    int v;
-    cin >> v;
-    int G[v][v];
-    for (int i = 0; i < v; i++)
-        for (int j = 0; j < v; j++)
-            G[i][j];
-
-    isBipartite(v, G, 0) ? cout << "Yes" : cout << "No";
+    fastio;
+    int n;
+    ifstream file;
+    file.open("input_2.txt");
+    if(!file){
+        cout<<"File not found";
+        return 0;
+    }
+    file>>n;
+    vector<vector<int>> graph(n,vector<int> (n,0));
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            file>>graph[i][j];
+    if(isbipartite(graph,0))
+        cout<<"Bipartite"<<endl;
+    else
+        cout<<"Not Bipartite"<<endl;
     return 0;
 }
