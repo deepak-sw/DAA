d3e8615d6eedb0d6191c9fa075f3352c52df159a
Deepak Singh Karki <87329265+deepak-sw@users.noreply.github.com>
2021-11-14 23:22:50 +0530

    Update Question2.cpp

--- Week10/Question2.cpp
+++ Week10/Question2.cpp
@@ -1,53 +1,56 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int find_maximum_jobs(
-    int N,
-    vector<pair<int, int> > ranges)
-{
-    // Min Heap
-    priority_queue<int, vector<int>,
-                   greater<int> >
-        queue;
-    sort(ranges.begin(), ranges.end());
-    int min_day = ranges[0].first;
-    int max_day = 0;
- 
-    for (int i = 0; i < N; i++)
-        max_day
-            = max(max_day,
-                  ranges[i].second);
- 
-    int index = 0, count_jobs = 0;
-    for (int i = min_day; i <= max_day; i++) {
-        while (index < ranges.size()
-               && ranges[index].first <= i) {
- 
-            queue.push(ranges[index].second);
-            index++;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input_2.txt", "r", stdin);
+    freopen("output_2.txt", "w", stdout);
+#endif
+    int n;
+    cin >> n;
+    vector < vector <int> > v(n, vector <int>(3));
+    for (int i = 0;i < n;i++) {
+        cin >> v[i][0];
+        v[i][2] = i;
+    }
+    for (int i = 0;i < n;i++) {
+        cin >> v[i][1];
+    }
+    sort(v.begin(), v.end(), [&](vector <int> a, vector <int> b) {
+        if (a[1] == b[2])
+            return a[0] < b[0];
+        return a[1] < b[1];
+        });
+    priority_queue < pair <int, int> > maxheap;
+    int current_time = 0;
+    vector <int> selected(n, 0);
+
+    for (int i = 0;i < n;i++) {
+
+        if (current_time + v[i][0] <= v[i][1]) {
+            current_time += v[i][0];
+            maxheap.push({ v[i][0] , v[i][2] });
+            selected[v[i][2]] = 1;
         }
-        while (!queue.empty()
-               && queue.top() < i)
-            queue.pop();
-        if (queue.empty())
-            continue;
-        count_jobs++;
-        queue.pop();
+        else if (maxheap.size()) {
+            if (current_time - maxheap.top().first + v[i][0] <= v[i][1] and maxheap.top().first > v[i][0]) {
+                selected[maxheap.top().second] = 0;
+                maxheap.pop();
+                current_time = current_time - maxheap.top().first + v[i][0];
+                selected[v[i][2]] = 1;
+                maxheap.push({ v[i][0] , v[i][2] });
+            }
+        }
     }
-    return count_jobs;
-}
-int main()
-{
- 
-    int N;
-    cin>>N;
-    vector<pair<int, int> > ranges;
-    int arr1[N], arr2[N];
-    for(int i=0;i<N;i++)
-        cin>>arr1[i];
-    for(int i=0;i<N;i++)
-        cin>>arr2[i];
-    for(int i=0;i<N;i++){
-        ranges.push_back(make_pair(arr1[i], arr2[i]));
+    int total = 0;
+    vector <int> ans;
+    for (int i = 0;i < n;i++) {
+        total += selected[i];
+
+        if (selected[i])
+            ans.push_back(i + 1);
     }
-    cout << find_maximum_jobs(N, ranges);
+    cout << "Max number of tasks = " << total << endl << "Selected task numbers : ";
+    for (int i : ans)
+        cout << i << ' ';
 }
