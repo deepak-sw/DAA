0be40bb57c64007c98a682c8fbf1f258af28eed1
Deepak Singh Karki <87329265+deepak-sw@users.noreply.github.com>
2021-11-14 21:49:15 +0530

    Update program3.cpp

--- Week2/program3.cpp
+++ Week2/program3.cpp
@@ -1,49 +1,40 @@
 #include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
 using namespace std;
-
-int search(int arr[], int lb, int ub, int ele)
-{
-    if (ub >= lb)
-    {
-        int mid = lb + (ub - lb) / 2;
-        if (ele == arr[mid])
-            return mid;
-        if (ele > arr[mid])
-            return search(arr, (mid + 1), ub, ele);
-        else
-            return search(arr, lb, (mid - 1), ele);
-    }
-    return -1;
-}
-
-int Fun(int arr[], int n, int k)
-{
-    int count = 0, i;
-    sort(arr, arr + n);
-    for (i = 0; i < n - 1; i++)
-        if (search(arr, i + 1, n - 1, arr[i] + k) != -1)
-            count++;
-
-    return count;
-}
 
 int main()
 {
-    // ios_base::sync_with_stdio(0);
-    // cin.tie(NULL);
-    int t, n, k;
-    cin >> t;
-    while (t--)
-    {
-        cin >> n;
+    fastio;
+    int t;
+    ifstream file;
+    file.open("input_3.txt");
+    if(!file){
+        cout<<"File not found";
+        return 0;
+    }
+    file>>t;
+    while(t--){
+        int n;
+        file>>n;
         int arr[n];
         for (int i = 0; i < n; i++)
-        {
-            cin >> arr[i];
+            file>>arr[i];
+        int k,count=0;
+        file>>k;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(abs(arr[i]-arr[j]) == k)
+                    count++;
+            }
         }
-
-        cin >> k;
-        cout << Fun(arr, n, k);
+        cout<<count<<endl;
     }
+    file.close();
     return 0;
 }
